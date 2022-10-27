f2352e25e5198cbdebed07fb03d19658c586218d
savannah030 <savannah030@naver.com>
2022-10-28 00:02:31 +0900

    initial commit

---
+++ src/test/java/com/gabozago/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabozago.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
