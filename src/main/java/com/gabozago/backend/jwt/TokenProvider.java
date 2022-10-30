e84e9647c93efea305ae82c257faff30b56957e9
nohkwanok <cloq@kakao.com>
2022-10-31 01:30:28 +0900

    Feat:토큰 검사 기능 추가
    🎨 TokenProvider @Autowired로 의존성 주입하도록 수정
    🎨 TokenProvider Filter에 쓸 메서드 추가 resolveToken(), getAuthentication()
    📰 UserRepository JPA 사용을 위한 리포지토리 파일 생성
    ☔️ 토큰 만료 테스트 삭제, getAuthentication 테스트 추가

--- src/main/java/com/gabozago/backend/jwt/TokenProvider.java
+++ src/main/java/com/gabozago/backend/jwt/TokenProvider.java
@@ -3,11 +3,18 @@ package com.gabozago.backend.jwt;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Base64;
 import java.util.Date;
 import java.util.List;
@@ -15,35 +22,31 @@ import java.util.List;
 @Component
 public class TokenProvider {
     private final Logger logger = LoggerFactory.getLogger(TokenProvider.class);
+    private final UserDetailsService userDetailsService;
+    private String secretKey = "secret";
 
-    private final String secretKey;
+    @Autowired
+    public TokenProvider(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
-    public TokenProvider(@Value("${jwt.secret}") String secret) {
-        this.secretKey = Base64.getEncoder().encodeToString(secret.getBytes());
+    @PostConstruct
+    protected void init() {
+        logger.info("Secret key: " + secretKey);
+        this.secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
     }
 
     public String createToken(Long userID, List<String> roles) {
         Claims claims = Jwts.claims().setSubject(userID.toString());
         claims.put("roles", roles);
 
-        // 7 days
-        long tokenLifeTime = 1000 * 60 * 60 * 24 * 7;
-
+        long expiration = 1000 * 60 * 60 * 24 * 7;
         return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + tokenLifeTime))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
-    }
-
-    public Long getUserID(String token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(secretKey)
-                .parseClaimsJws(token)
-                .getBody();
-
-        return Long.parseLong(claims.getSubject());
     }
 
     public Boolean validateToken(String token) {
@@ -54,5 +57,26 @@ public class TokenProvider {
             logger.error("Invalid JWT token: {}", e.getMessage());
         }
         return false;
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    public Authentication getAuthentication(String token) {
+        if (!validateToken(token)) {
+            return null;
+        }
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUserIdFromToken(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    private String getUserIdFromToken(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
     }
 }
--- src/test/java/com/gabozago/backend/jwt/TokenProviderTests.java
+++ src/test/java/com/gabozago/backend/jwt/TokenProviderTests.java
@@ -1,36 +1,54 @@
 package com.gabozago.backend.jwt;
 
-
+import ch.qos.logback.classic.util.ContextInitializer;
+import com.gabozago.backend.entity.User;
+import com.gabozago.backend.service.UserService;
+import org.apache.tomcat.util.file.ConfigFileLoader;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+@DisplayName("Token Provider Tests")
 public class TokenProviderTests {
     TokenProvider tokenProvider;
+    User user;
 
     @BeforeEach
     public void init() {
-        tokenProvider = new TokenProvider("secret");
+        user = User.builder().id(1L).email("test@example.com").username("user").password("password").build();
+        tokenProvider = new TokenProvider(new UserService(null) {
+            @Override
+            public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+                return user;
+            }
+
+            @Override
+            public void save(User user) {
+                // do nothing
+            }
+
+            @Override
+            public User findByEmail(String email) {
+                return user;
+            }
+        });
+
+
     }
 
     @Test
     void testCreateToken() {
-        String token = tokenProvider.createToken(1L, null);
+        tokenProvider.createToken(1L, null);
     }
 
     @Test
     void testCreateTokenWithRoles() {
-        String token = tokenProvider.createToken(1L, List.of("ROLE_USER"));
-    }
-
-    @Test
-    void testGetUserID() {
-        String token = tokenProvider.createToken(1L, null);
-        Long userID = tokenProvider.getUserID(token);
-        assertEquals(1L, userID);
+        tokenProvider.createToken(1L, List.of("ROLE_USER"));
     }
 
     @Test
@@ -44,6 +62,18 @@ public class TokenProviderTests {
     void testValidateTokenWithInvalidToken() {
         Boolean isValid = tokenProvider.validateToken("invalid token");
         assertEquals(false, isValid);
+    }
+
+    @Test
+    void testGetAuthentication() {
+        System.out.println(user.getId());
+        String token = tokenProvider.createToken(user.getId(), null);
+        tokenProvider.getAuthentication(token);
+    }
+
+    @Test
+    void testGetAuthenticationWithInvalidToken() {
+        tokenProvider.getAuthentication("invalid token");
     }
 }
 
