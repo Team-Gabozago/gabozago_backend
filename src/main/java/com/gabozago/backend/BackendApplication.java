7443ea802fb92877c179783e625a57e5d701f500
savannah030 <savannah030@naver.com>
2022-11-02 16:46:29 +0900

    📰feat: test 페이지 생성

--- src/main/java/com/gabozago/backend/BackendApplication.java
+++ src/main/java/com/gabozago/backend/BackendApplication.java
@@ -6,10 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@GetMapping("/test")
+	public String test(){
+		return "test";
+	}
 }
