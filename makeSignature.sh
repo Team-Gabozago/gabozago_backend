744d32349f0caa30928f33272ccc80c053bfdf5e
savannah030 <savannah030@naver.com>
2022-11-09 00:31:43 +0900

    🚀Feat: #14 OUTPUT 출력해보기

--- makeSignature.sh
+++ makeSignature.sh
@@ -3,7 +3,8 @@ function makeSignature() {
   # make signiture
 	nl=$'\\n'
 
-	TIMESTAMP=$(echo $(($(date +%s%N)/1000000)))
+	#TIMESTAMP=$(echo $(($(date +%s%N)/1000000)))
+	TIMESTAMP="1505290625682"
 	ACCESSKEY=$1				# access key id (from portal or Sub Account)
 	SECRETKEY=$2				# secret key (from portal or Sub Account)
 
@@ -16,7 +17,8 @@ function makeSignature() {
 
 	SIGNATURE=$(echo -n -e "$SIG"|iconv -t utf8 |openssl dgst -sha256 -hmac $SECRETKEY -binary|openssl enc -base64)
 
-	echo ${TIMESTAMP}, ${SIGNATURE}
+	#echo ${TIMESTAMP}, ${SIGNATURE}
+	echo ${TIMESTAMP}
 
 
 }
