3d9075d83fc391b244572107dc1369a39c3d0f3e
savannah030 <savannah030@naver.com>
2022-11-08 16:30:37 +0900

    🚀Feat: #14 requestDeploy.sh 생성(하드코딩)

---
+++ requestDeploy.sh
@@ -0,0 +1,29 @@
+function requestDeploy() {
+
+  # make signiture
+	nl=$'\\n'
+
+	TIMESTAMP=$(echo $(($(date +%s%N)/1000000)))
+	ACCESSKEY="F69B46A5BD74794E8737"				# access key id (from portal or Sub Account)
+	SECRETKEY="EFF9C27D067B780DF3E9C5707695DF66A46681EF"				# secret key (from portal or Sub Account)
+
+	METHOD="GET"
+	URI="https://sourcedeploy.apigw.ntruss.com/api/v1/project"
+
+	SIG="$METHOD"' '"$URI"${nl}
+	SIG+="$TIMESTAMP"${nl}
+	SIG+="$ACCESSKEY"
+
+	SIGNATURE=$(echo -n -e "$SIG"|iconv -t utf8 |openssl dgst -sha256 -hmac $SECRETKEY -binary|openssl enc -base64)
+
+  echo $SIGNATURE
+
+	# sourceDeploy request
+	curl --request GET \
+            --url https://sourcedeploy.apigw.ntruss.com/api/v1/project \
+            --header 'x-ncp-apigw-timestamp:'$ACCESSKEY \
+            --header 'x-ncp-iam-access-key:'$SECRETKEY  \
+            --header 'x-ncp-apigw-signature-v2:'$SIGNATURE \
+            --header 'x-ncp-region_code: "KR" '
+
+}
