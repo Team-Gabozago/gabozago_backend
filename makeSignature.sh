300da2185295e2ce191d063ccf14b9bb1609852d
savannah030 <savannah030@naver.com>
2022-11-08 17:29:04 +0900

    🚀Feat: #14 secrets.NCP_ACCESS_KEY_ID_DEPLOY 숨기기

--- makeSignature.sh
+++ makeSignature.sh
@@ -4,8 +4,8 @@ function makeSignature() {
 	nl=$'\\n'
 
 	TIMESTAMP=$(echo $(($(date +%s%N)/1000000)))
-	ACCESSKEY="F69B46A5BD74794E8737"				# access key id (from portal or Sub Account)
-	SECRETKEY="EFF9C27D067B780DF3E9C5707695DF66A46681EF"				# secret key (from portal or Sub Account)
+	ACCESSKEY=$1				# access key id (from portal or Sub Account)
+	SECRETKEY=$2				# secret key (from portal or Sub Account)
 
 	METHOD="GET"
 	URI="https://sourcedeploy.apigw.ntruss.com/api/v1/project"
