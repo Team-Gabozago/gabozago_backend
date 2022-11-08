37b435a24e1b88be82fa926d5fe1bfd16d493d52
savannah030 <savannah030@naver.com>
2022-11-08 17:20:07 +0900

    🚀Feat: #14 SIGNATURE 제대로 생성되는지 확인

--- makeSignature.sh
+++ makeSignature.sh
@@ -15,5 +15,5 @@ function makeSignature() {
 	SIG+="$ACCESSKEY"
 
 	SIGNATURE=$(echo -n -e "$SIG"|iconv -t utf8 |openssl dgst -sha256 -hmac $SECRETKEY -binary|openssl enc -base64)
-
+  echo "${SIGNATURE}"
 }
