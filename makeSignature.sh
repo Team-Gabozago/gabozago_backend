62917312eb2576f562210c73a20bf184a5a457a5
savannah030 <savannah030@naver.com>
2022-11-09 00:21:10 +0900

    🚀Feat: #14 OUTPUT 출력해보기

--- makeSignature.sh
+++ makeSignature.sh
@@ -16,7 +16,7 @@ function makeSignature() {
 
 	SIGNATURE=$(echo -n -e "$SIG"|iconv -t utf8 |openssl dgst -sha256 -hmac $SECRETKEY -binary|openssl enc -base64)
 
-	echo "$TIMESTAMP"'\n'"$SIGNATURE"
+	echo ${TIMESTAMP}, ${SIGNATURE}
 
 
 }
