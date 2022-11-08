35ddad8090a53d3bd9769ccc12fd19854c0669e2
savannah030 <savannah030@naver.com>
2022-11-08 17:01:21 +0900

    🚀Feat: #14 배포 요청은 github actions에서 보내도록 변경

--- requestDeploy.sh
+++ requestDeploy.sh
@@ -1,4 +1,4 @@
-function requestDeploy() {
+function makeSignature() {
 
   # make signiture
 	nl=$'\\n'
@@ -15,15 +15,5 @@ function requestDeploy() {
 	SIG+="$ACCESSKEY"
 
 	SIGNATURE=$(echo -n -e "$SIG"|iconv -t utf8 |openssl dgst -sha256 -hmac $SECRETKEY -binary|openssl enc -base64)
-
-  echo $SIGNATURE
-
-	# sourceDeploy request
-	curl --request GET \
-            --url https://sourcedeploy.apigw.ntruss.com/api/v1/project \
-            --header 'x-ncp-apigw-timestamp:'$ACCESSKEY \
-            --header 'x-ncp-iam-access-key:'$SECRETKEY  \
-            --header 'x-ncp-apigw-signature-v2:'$SIGNATURE \
-            --header 'x-ncp-region_code: "KR" '
 
 }
