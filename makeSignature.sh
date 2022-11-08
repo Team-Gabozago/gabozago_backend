c32b6904466a19fe19790902d1ec84d5d5422337
savannah030 <savannah030@naver.com>
2022-11-09 01:01:10 +0900

    🚀Feat: #14 makeSignature 함수 왜 리턴안될까

--- makeSignature.sh
+++ makeSignature.sh
@@ -18,7 +18,8 @@ function makeSignature() {
 	SIGNATURE=$(echo -n -e "$SIG"|iconv -t utf8 |openssl dgst -sha256 -hmac $SECRETKEY -binary|openssl enc -base64)
 
 	#echo ${TIMESTAMP}, ${SIGNATURE}
-	echo $TIMESTAMP
+	#echo $TIMESTAMP
+	echo "${TIMESTAMP}"
 
 
 }
