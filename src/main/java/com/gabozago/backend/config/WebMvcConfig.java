a695b97a8d19e8be70bd4e61e1b0ee06c0b04689
nohkwanok <cloq@kakao.com>
2022-11-19 13:09:27 +0900

    Feat: #29 CORS 이슈 대응

---
+++ src/main/java/com/gabozago/backend/config/WebMvcConfig.java
@@ -0,0 +1,30 @@
+package com.gabozago.backend.config;
+
+import com.gabozago.backend.jwt.CorsFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    long MAX_AGE_SECS = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
+    }
+
+
+
+    @Bean
+    public FilterRegistrationBean<?> filterBean() {
+        return new FilterRegistrationBean<>(new CorsFilter());
+    }
+}
