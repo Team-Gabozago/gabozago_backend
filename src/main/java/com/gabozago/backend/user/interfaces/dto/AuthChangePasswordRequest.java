1b35fbd07f89feb50a9b9233853d682238e3cc40
kwanok <cloq@kakao.com>
2022-11-30 15:18:43 +0900

    🛠️Fix: #59 패스워드 변경 파라미터 검증 추가

--- src/main/java/com/gabozago/backend/user/interfaces/dto/AuthChangePasswordRequest.java
+++ src/main/java/com/gabozago/backend/user/interfaces/dto/AuthChangePasswordRequest.java
@@ -2,11 +2,15 @@ package com.gabozago.backend.user.interfaces.dto;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 public class AuthChangePasswordRequest {
     public static final String PASSWORD_NOT_NULL = "password는 필수 입력값입니다.";
     public static final String NEW_PASSWORD_NOT_NULL = "newPassword는 필수 입력값입니다.";
 
+    @NotBlank(message = PASSWORD_NOT_NULL)
     private String password;
+    @NotBlank(message = NEW_PASSWORD_NOT_NULL)
     private String newPassword;
 }
