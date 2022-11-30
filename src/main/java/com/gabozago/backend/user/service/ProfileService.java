87741d63a2a53cc749b72ea32d5bd6e2a7168f4d
kwanok <cloq@kakao.com>
2022-11-30 14:55:40 +0900

    📝Feat: #57 유저 이미지 업로드 s3로 수정

--- src/main/java/com/gabozago/backend/user/service/ProfileService.java
+++ src/main/java/com/gabozago/backend/user/service/ProfileService.java
@@ -7,13 +7,14 @@ import com.gabozago.backend.feed.domain.Like;
 import com.gabozago.backend.feed.infrastructure.CommentRepository;
 import com.gabozago.backend.feed.infrastructure.FeedRepository;
 import com.gabozago.backend.feed.infrastructure.LikeRepository;
+import com.gabozago.backend.user.domain.ProfileImage;
 import com.gabozago.backend.user.domain.User;
+import com.gabozago.backend.user.infrastructure.ProfileImageRepository;
 import com.gabozago.backend.user.infrastructure.UserRepository;
 import com.gabozago.backend.user.interfaces.dto.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,6 +27,7 @@ public class ProfileService {
     private final CommentRepository commentRepository;
 
     private final LikeRepository likeRepository;
+    private final ProfileImageRepository profileImageRepository;
 
     public void update(User user, ProfileUpdateRequest request) {
         if (userRepository.findByNickname(request.getNickname()).isPresent()) {
@@ -71,5 +73,13 @@ public class ProfileService {
                 .collect(Collectors.toList());
 
         return ProfileLikesResponse.of("좋아요를 성공적으로 조회했습니다", "LIKE_FETCHED", profileLikeResponses);
+    }
+
+    public ProfileImageUploadResponse saveProfileImage(ProfileImage profileImage, User user) {
+        profileImageRepository.save(profileImage);
+        user.updateProfileImage(profileImage);
+        userRepository.save(user);
+
+        return ProfileImageUploadResponse.of(profileImage);
     }
 }
