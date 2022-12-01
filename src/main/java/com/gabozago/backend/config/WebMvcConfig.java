ee53395eb203f723527340929455d19c3b8bfac5
kwanok noh <61671343+kwanok@users.noreply.github.com>
2022-12-01 13:12:43 +0900

    🚑 Fix: allowedOrigins 추가

--- src/main/java/com/gabozago/backend/config/WebMvcConfig.java
+++ src/main/java/com/gabozago/backend/config/WebMvcConfig.java
@@ -14,7 +14,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://wontu.site")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
