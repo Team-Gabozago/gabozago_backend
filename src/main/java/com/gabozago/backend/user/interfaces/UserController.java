ff8cc2985fa45c847e163bc733166853b0992dc2
nohkwanok <cloq@kakao.com>
2022-12-04 18:11:27 +0900

    🎨Feat #71 유저 현재 구역 조회 api 적용

--- src/main/java/com/gabozago/backend/user/interfaces/UserController.java
+++ src/main/java/com/gabozago/backend/user/interfaces/UserController.java
@@ -1,9 +1,13 @@
 package com.gabozago.backend.user.interfaces;
 
 
+import com.gabozago.backend.common.exception.EntityNotFoundException;
+import com.gabozago.backend.common.kakaomap.KakaoMapService;
+import com.gabozago.backend.common.response.ErrorCode;
 import com.gabozago.backend.user.domain.User;
 import com.gabozago.backend.user.infrastructure.UserRepository;
-import com.gabozago.backend.user.interfaces.dto.UserAreasResponse;
+import com.gabozago.backend.user.interfaces.dto.UserGetAreaResponse;
+import com.gabozago.backend.user.interfaces.dto.UserGetAreasResponse;
 import com.gabozago.backend.user.interfaces.dto.user.GetMeResponseDto;
 import com.gabozago.backend.user.service.AreaService;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +23,8 @@ public class UserController {
     private final UserRepository userRepository;
 
     private final AreaService areaService;
+
+    private final KakaoMapService kakaoMapService;
 
     @GetMapping("/me")
     public ResponseEntity<String> getMe(@AuthenticationPrincipal User user) {
@@ -43,7 +49,16 @@ public class UserController {
     }
 
     @GetMapping("/areas")
-    public ResponseEntity<UserAreasResponse> getAreas(@AuthenticationPrincipal User user){
+    public ResponseEntity<UserGetAreasResponse> getAreas(@AuthenticationPrincipal User user){
         return ResponseEntity.ok(areaService.findAll());
+    }
+
+    @GetMapping("/area")
+    public ResponseEntity<UserGetAreaResponse> getArea(@AuthenticationPrincipal User user){
+        if (!user.hasLocation()) {
+            throw new EntityNotFoundException(ErrorCode.USER_LOCATION_NOT_FOUND);
+        }
+
+        return ResponseEntity.ok(UserGetAreaResponse.of("유저의 구역 조회가 완료됐습니다.", kakaoMapService.getPlaceName(user.getLatitude(), user.getLongitude())));
     }
 }
