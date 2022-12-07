4dd14d8ea9904fd6d4a78e8f1b30e1157b7df903
심은지 <simbomi06@gmail.com>
2022-12-07 12:27:24 +0900

    fix 주소관련 테스트 코드 임시 주석 처리

--- src/test/java/com/gabozago/backend/fixture/LocationFixture.java
+++ src/test/java/com/gabozago/backend/fixture/LocationFixture.java
@@ -1,11 +1,11 @@
-package com.gabozago.backend.fixture;
-
-import com.gabozago.backend.feed.domain.Location;
-
-public class LocationFixture {
-
-    public static final Location A_STATION = new Location(127.10016437214135,
-            37.51337688858131);
-
-    public static final Location B_STATION = new Location(127.027591, 37.497962);
-}
+//package com.gabozago.backend.fixture;
+//
+//import com.gabozago.backend.feed.domain.Location;
+//
+//public class LocationFixture {
+//
+//    public static final Location A_STATION = new Location(127.10016437214135,
+//            37.51337688858131);
+//
+//    public static final Location B_STATION = new Location(127.027591, 37.497962);
+//}
