2eace7035ce66b8f7b449e67919a56ba379a312b
kwanok <cloq@kakao.com>
2022-12-23 15:06:37 +0900

    🎨Feat: #95 /profile/comments 피드 내용도 같이 가져오기
    comment 객체 안에
    대댓글인 경우: 부모 댓글을 parent_content로 가져오기, 피드 내용은 feed_content로 가져오기
    댓글인 경우: 부모 댓글은 null처리

--- src/main/java/com/gabozago/backend/profile/interfaces/dto/CommentResponse.java
+++ src/main/java/com/gabozago/backend/profile/interfaces/dto/CommentResponse.java
@@ -12,26 +12,38 @@ import java.time.LocalDateTime;
 public class CommentResponse {
     private final Long id;
     private final String content;
+    private final String feedContent;
+    private final String parentContent;
     private final boolean feedAuthor;
-    private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
     private final boolean modified;
     private final Long feedId;
 
-    public CommentResponse(Long id, String content, boolean feedAuthor, LocalDateTime createdAt, boolean modified, Long feedId) {
+    public CommentResponse(Long id, String content, String feedContent, String parentContent, boolean feedAuthor, LocalDateTime updatedAt, boolean modified, Long feedId) {
         this.id = id;
         this.content = content;
+        this.feedContent = feedContent;
+        this.parentContent = parentContent;
         this.feedAuthor = feedAuthor;
-        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
         this.modified = modified;
         this.feedId = feedId;
     }
 
     public static CommentResponse of(Comment comment) {
+        Comment parent = comment.getParent();
+        String parentContent = null;
+        if (parent != null) {
+            parentContent = parent.getContent();
+        }
+
         return new CommentResponse(
                 comment.getId(),
                 comment.getContent(),
+                comment.getFeed().getContent(),
+                parentContent,
                 comment.isFeedAuthor(),
-                comment.getCreatedAt(),
+                comment.getUpdatedAt(),
                 comment.isModified(),
                 comment.getFeed().getId()
         );
